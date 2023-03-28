ad42ecde6249ffe67cd64f8cef8b0d8f0e9706f4
Артем Дяченко <ADyachenko3@alfabank.ru>
2023-03-28 20:23:43 +0300

    add third rows

--- AnotherTestWebinar.java
+++ AnotherTestWebinar.java
@@ -3,5 +3,6 @@ public class AnotherTestWebinar {
     public static void main(String[] args) {
         System.out.println("Another");
         System.out.println("Second row");
+        System.out.println("Third row");
     }
 }
