97a9fb6dfdc17ec9e95b16a38e0ad8a04121d8e0
Артем Дяченко <ADyachenko3@alfabank.ru>
2023-03-28 20:22:18 +0300

    add second rows

--- AnotherTestWebinar.java
+++ AnotherTestWebinar.java
@@ -2,5 +2,6 @@ public class AnotherTestWebinar {
 
     public static void main(String[] args) {
         System.out.println("Another");
+        System.out.println("Second row");
     }
 }
--- TestWebinar.java
+++ TestWebinar.java
@@ -2,5 +2,6 @@ public class TestWebinar {
 
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Second");
     }
 }
